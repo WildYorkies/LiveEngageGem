376eca2576e95f047808ee67cffb7832411bbdaf
Cale Short <cale.short@gmail.com>
2016-09-19 12:25:03 -0400

    change all methods to have keyword arguments. ensure that all api's are getting their base URI's correctly

--- liveengage_gem.rb
+++ liveengage_gem.rb
@@ -9,27 +9,29 @@ require 'net/https'
 #response = JSON.parse(json_response.body)
 
 class Service
-    def initialize(service_name)
+    def initialize(name:)
         @base_uri = ''
-        @name = service_name
+        @name = name
     end
-    def get_base_uri(app)
+    def get_base_uri(app:)
         uri = URI.parse("https://api.liveperson.net/api/account/#{app.account_id}/service/#{@name}/baseURI.json?version=1.0")
         request = Net::HTTP::Get.new(uri.request_uri)
-        #request.add_field['authorization'] = oauth_sig  --if you need to add the sig 
+        #request.add_field['authorization'] = app.oauth  --if you need to add the sig 
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true
         #http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
         response = http.start do |client|
             client.request(request)
         end
         data_hash = JSON.parse(response.body)
         @base_uri = data_hash['baseURI']
+        self
     end
     def to_s
         "#{self.class}\n\t#{@name} => #{@base_uri}"
     end
-    def request(app, method, request_uri = nil, params = nil)
+    def request(app:, method:, request_uri: nil, params: nil)
         uri = URI.parse(@base_uri)
         case method
         when :get
@@ -48,10 +50,10 @@ end
 
 class EngagementHistory < Service
     def initialize
-        super('engHistDomain')
+        super(name: 'engHistDomain')
     end
-    def request(app, method, params=nil)
-        super(app, :post, '/enghist/interactions') do |client, request|
+    def request(app:, method:, params: nil)
+        super(app: app, method: :post, params: '/enghist/interactions') do |client, request|
             
         end
     end
@@ -59,10 +61,10 @@ end
 
 class OperationalRealTime < Service
     def initialize
-        super('leDataReporting')
+        super(name: 'leDataReporting')
     end
-    def request(app, method, params=nil)
-        super(app, :get, '/rtapi/details/') do |client, request|
+    def request(app:, method:, params: nil)
+        super(app: app, method: :get, params: '/rtapi/details/') do |client, request|
             
         end
     end
@@ -70,16 +72,16 @@ end
 
 class IvrDeflectionEngagement < Service
     def initialize
-        super('smt')
+        super(name: 'smt')
     end
 end
 
 class UserManagement < Service
-    def initialize(read_only = false)
+    def initialize(read_only: false)
         if read_only
-            super('accountConfigReadOnly')
+            super(name: 'accountConfigReadOnly')
         else
-            super('accountConfigReadWrite')
+            super(name: 'accountConfigReadWrite')
         end
     end
 end
@@ -95,7 +97,7 @@ end
 
 class Application
     attr_reader :tokens, :account_id
-    def initialize(account_id, tokens)
+    def initialize(account_id:, tokens:)
         @account_id = account_id
         @tokens = tokens
     end
@@ -103,19 +105,23 @@ class Application
         consumer = OAuth::Consumer.new(@tokens[:consumer_key], @tokens[:consumer_secret])
         accesstoken = OAuth::AccessToken.new(consumer, @tokens[:access_token], @tokens[:access_token_secret])
         return accesstoken
-        
         # call accesstoken.sign!(request, ) inside of the using method
-
-
-
     end
 end
 
-app = Application.new('89119334', {consumer_key: 'fdsafdsa', 
-                                   consumer_secret: 'fdsafdsa', 
-                                   access_token: 'fdsafdsa', 
-                                   access_token_secret: 'fdsafdsa'})
-p app.oauth 
-users_api = Users.new
-users_api.get_base_uri(app)
-puts users_api
+app = Application.new(account_id: 'xxx', tokens: {consumer_key: 'fdsafdsa', 
+                                                       consumer_secret: 'fdsafdsa', 
+                                                       access_token: 'fdsafdsa', 
+                                                       access_token_secret: 'fdsafdsa'})
+
+# TESTS 
+p app, app.oauth
+
+users_api = Users.new.get_base_uri(app: app)
+agents_api = Agents.new.get_base_uri(app: app)
+agent_groups_api = AgentGroups.new.get_base_uri(app: app)
+ivr_deflection_api = IvrDeflectionEngagement.new.get_base_uri(app: app)
+operational_api = OperationalRealTime.new.get_base_uri(app: app)
+engagement_history_api = EngagementHistory.new.get_base_uri(app: app)
+
+puts users_api, agents_api, agent_groups_api, ivr_deflection_api, operational_api, engagement_history_api
