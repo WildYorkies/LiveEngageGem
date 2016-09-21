b910ed941d281555fab0742abd1c551dfe284660
Cale <cale.short@gmail.com>
2016-09-21 15:55:58 -0400

    change get_base_uri to use open-uri
     change get_base_uri to use open-uri for more simple implementation
    
    remove UserManagement abstract class for more direct code

--- liveengage_gem.rb
+++ liveengage_gem.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'oauth'
 require 'json'
 require 'net/https'
+require 'open-uri'
 
 #consumer = OAuth::Consumer.new(consumer_key, consumer_secret)
 #accesstoken = OAuth::AccessToken.new(consumer, access_token, access_token_secret)
@@ -13,25 +14,16 @@ class Service
         @base_uri = ''
         @name = name
     end
-    def get_base_uri(app:)
-        uri = URI.parse("https://api.liveperson.net/api/account/#{app.account_id}/service/#{@name}/baseURI.json?version=1.0")
-        request = Net::HTTP::Get.new(uri.request_uri)
-        #request.add_field['authorization'] = app.oauth  --if you need to add the sig 
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true
-        #http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        response = http.start do |client|
-            client.request(request)
-        end
-        data_hash = JSON.parse(response.body)
-        @base_uri = data_hash['baseURI']
+    def get_base_uri(account_id:)
+        response = open("https://api.liveperson.net/api/account/#{account_id}/service/#{@name}/baseURI.json?version=1.0").read
+        data = JSON.parse(response.body)
+        @base_uri = data['baseURI']
         self
     end
     def to_s
         "#{self.class}\n\t#{@name} => #{@base_uri}"
     end
-    def request(app:, method:, request_uri: nil, params: nil)
+    def request(token:, method:, request_uri: nil, params: nil)
         uri = URI.parse(@base_uri)
         case method
         when :get
@@ -39,7 +31,7 @@ class Service
         when :post
             request = Net::HTTP::Post.new(request_uri)
         end
-        app.oauth.sign!(request)
+        token.sign!(request)
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true
         response = http.start do |client|
@@ -52,21 +44,11 @@ class EngagementHistory < Service
     def initialize
         super(name: 'engHistDomain')
     end
-    def request(app:, method:, params: nil)
-        super(app: app, method: :post, params: '/enghist/interactions') do |client, request|
-            
-        end
-    end
 end
 
 class OperationalRealTime < Service
     def initialize
         super(name: 'leDataReporting')
-    end
-    def request(app:, method:, params: nil)
-        super(app: app, method: :get, params: '/rtapi/details/') do |client, request|
-            
-        end
     end
 end
 
@@ -76,23 +58,22 @@ class IvrDeflectionEngagement < Service
     end
 end
 
-class UserManagement < Service
+class Users < Service
     def initialize(read_only: false)
-        if read_only
-            super(name: 'accountConfigReadOnly')
-        else
-            super(name: 'accountConfigReadWrite')
-        end
+        read_only ? super(name: 'accountConfigReadOnly') : super(name: 'accountConfigReadWrite')
     end
 end
 
-class Users < UserManagement
+class Agents < Service
+    def initialize(read_only: false)
+        read_only ? super(name: 'accountConfigReadOnly') : super(name: 'accountConfigReadWrite')
+    end
 end
 
-class Agents < UserManagement
-end
-
-class AgentGroups < UserManagement
+class AgentGroups < Service
+    def initialize(read_only: false)
+        read_only ? super(name: 'accountConfigReadOnly') : super(name: 'accountConfigReadWrite')
+    end
 end
 
 class Application
@@ -117,11 +98,11 @@ app = Application.new(account_id: 'xxx', tokens: {consumer_key: 'fdsafdsa',
 # TESTS 
 p app, app.oauth
 
-users_api = Users.new.get_base_uri(app: app)
-agents_api = Agents.new.get_base_uri(app: app)
-agent_groups_api = AgentGroups.new.get_base_uri(app: app)
-ivr_deflection_api = IvrDeflectionEngagement.new.get_base_uri(app: app)
-operational_api = OperationalRealTime.new.get_base_uri(app: app)
-engagement_history_api = EngagementHistory.new.get_base_uri(app: app)
+users_api = Users.new(read_only: true).get_base_uri(account_id: app.account_id)
+agents_api = Agents.new.get_base_uri(account_id: app.account_id)
+agent_groups_api = AgentGroups.new.get_base_uri(account_id: app.account_id)
+ivr_deflection_api = IvrDeflectionEngagement.new.get_base_uri(account_id: app.account_id)
+operational_api = OperationalRealTime.new.get_base_uri(account_id: app.account_id)
+engagement_history_api = EngagementHistory.new.get_base_uri(account_id: app.account_id)
 
 puts users_api, agents_api, agent_groups_api, ivr_deflection_api, operational_api, engagement_history_api
